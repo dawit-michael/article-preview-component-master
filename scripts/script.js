b6d1d6883f00edbba7cae2a07ec3a6ccfe56963d
Dawit Michael <dave.mic1227@gmail.com>
2020-11-24 11:39:05 +0300

    script done

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-  $("#shareBtn").click(function () {
-    $(".tooltip-content").addClass("active");
+  anime({
+    targets: ".container",
+    easing: "easeOutExpo",
+    translateY: [-50, 0],
+    duration: 3000,
+    loop: false,
+  });
+  $("#shareBtn").click(function (e) {
+    $(".tooltip-content").toggleClass("active");
+    e.preventDefault();
   });
 });
